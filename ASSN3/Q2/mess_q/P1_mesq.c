11a1b035182a54eb0932d74b48a0d0021c3e91ba
r79007 <rohan20537@iiitd.ac.in>
2021-12-06 13:40:34 +0530

    working_on_mesq

---
+++ ASSN3/Q2/mess_q/P1_mesq.c
@@ -0,0 +1,139 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include<errno.h>
+#include <stdint.h>
+#include<fcntl.h>
+
+#define PROJECT_PATHNAME "P1_mesq.c"
+#define PROJECT_ID 57
+#define MSG_SIZE 408
+
+struct msg_struct
+{
+    long type;
+    char msg[50][8];
+    char maxId[10];
+}message;
+
+int rdrand16_step (uint16_t *rand)
+{
+	unsigned char ok;
+
+	asm volatile ("rdrand %0; setc %1"
+		: "=r" (*rand), "=qm" (ok));
+
+	return (int) ok;
+}
+
+char *gen_random(size_t length) { // const size_t length, supra
+
+static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; // could be const
+char *randomString;
+
+if (length) {
+    randomString = malloc(length +1); // sizeof(char) == 1, cf. C99
+
+    if (randomString) {
+        int l = (int) (sizeof(charset) -1); // (static/global, could be const or #define SZ, would be even better)
+        int key;  // one-time instantiation (static/global would be even better)
+        for (int n = 0;n < length;n++) {
+            uint16_t rndnum;
+            rdrand16_step(&rndnum);        
+            key = rndnum % l;   // no instantiation, just assignment, no overhead from sizeof
+            randomString[n] = charset[key];
+        }
+
+        randomString[length] = '\0';
+    }
+}
+
+return randomString;
+}
+
+int main(){
+    
+    char* buffer[50];    
+    for(int i=0;i<50;i++){               //Storing 50 random strings of length 4
+        buffer[i] = gen_random(4);
+        //printf("%s\n", buffer[i]);
+    }
+
+
+    // * Generate unique key to access the message queue
+    key_t key = ftok(PROJECT_PATHNAME, PROJECT_ID);
+    if(key==-1)
+    {
+        printf("Error generating msg key!\n%s\n", strerror(errno));
+        return -1;
+    }
+
+    // * Create queue in kernel or get the id for an existing queue
+    int qid = msgget(key, 0666|IPC_CREAT);//? Why 0666?
+    if(qid==-1)
+    {
+        printf("Error retrieving queue id!\n%s\n", strerror(errno));
+        return -1;
+    }
+
+    message.type=1;
+
+    char* buf[50];
+    int strs=0;
+        for(;;){
+            for(int j=0;j<5;j++){               //Storing 50 random strings of length 4, along with indices
+            char* str = buffer[strs];
+            //printf("%s\n", buffer[i]);
+            char buff[50];
+            sprintf(buff,"%s %d",str,strs);
+            buf[strs]=buff;
+            char* string=buf[strs];
+            strcpy(message.msg[strs],buff);
+            
+            //string=buf[i];
+            //printf("%d\n", atoi(&string[5]));
+            //msgsnd(qid, (int *) &message.a[i], sizeof(message.a[i]), 0);
+            msgsnd(qid,&message,sizeof(message),0);
+            printf("Data sent is : %s\n", message.msg[strs]);
+            
+                strs++;
+                if(strs>=50){
+                    break;
+                }
+            }
+
+            struct msqid_ds buf2;
+    
+do
+    {
+        if(msgrcv(qid, &message, sizeof(message),1,0)==-1)
+        {
+            printf("Error recieving message!\n%s\n", strerror(errno));
+            return -1;
+        }
+        //printf("%s(%ld) ", message.msg, message.type);
+        
+        if(msgctl(qid, IPC_STAT, &buf2) == -1) // * check queue status
+        {
+            printf("Error retrieving msgctl info!\n%s\n", strerror(errno));
+            return -1;
+        }
+    }while(buf2.msg_qnum!=0);
+
+            
+            //int nread=read(data_socket,maxId,4);
+            strs=atoi(message.maxId)+1;
+            
+}
+        //wait(1);
+        
+        
+
+
+
+    return 0;
+}
--- ASSN3/Q2/mess_q/P2_mesq.c
+++ ASSN3/Q2/mess_q/P2_mesq.c
@@ -7,7 +7,7 @@
 #include <sys/msg.h>
 #include<errno.h>
 
-#define PROJECT_PATHNAME "P1_mesq.c"
+#define PROJECT_PATHNAME "p1_mesq.c"
 #define PROJECT_ID 57
 #define MSG_SIZE 512
 
@@ -16,12 +16,13 @@ struct msg_struct
 {
     long type;
     char msg[50][8];
+    char maxId[10];
 }message;
 
 
 int main()
 {
-    
+    int k=0;
     // * set up message queue
 
     // * Generate unique key to access the message queue
@@ -48,13 +49,17 @@ int main()
     
     //char* last=buf->msg.msg_last;
     // * Receive messages while the queue is not empty
-
+    while(k<50){
     do
     {
         if(msgrcv(qid, &message, sizeof(message),1,0)==-1)
         {
             printf("Error recieving message!\n%s\n", strerror(errno));
             return -1;
+        }else{
+            printf("String Received is : %s and maxId is %s and id is %s\n", message.msg[k],message.maxId,message.msg[k]+5);
+            sprintf(message.maxId,"%s",message.msg[k]+5);
+            k++;
         }
         //printf("%s(%ld) ", message.msg, message.type);
         
@@ -64,22 +69,23 @@ int main()
             return -1;
         }
     }while(buf.msg_qnum!=0);
-    printf("\n");
+    //printf("\n");
     
-
     
+    msgsnd(qid,&message,sizeof(message),0);
 
+}
     
     // for(int i=0; i<50; i++){
     //     msgrcv(qid, &message, sizeof(message),1,0);
     // }      
     
     
-    for(int i=0;i<50;i++){
-        char* string=message.msg[i];
-        printf("Data Received is : %s and id is %d\n", message.msg[i],atoi(&string[5])); 
-        //printf("Data Received is : %s\n", message.msg[i]);
-    }
+    // for(int i=0;i<50;i++){
+    //     char* string=message.msg[i];
+    //     printf("Data Received is : %s and id is %d\n", message.msg[i],atoi(&string[5])); 
+    //     //printf("Data Received is : %s\n", message.msg[i]);
+    // }
 //28109
     msgctl(qid, IPC_RMID, NULL); // * remove message queue
 
